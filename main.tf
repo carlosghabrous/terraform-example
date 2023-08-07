7532fd8b4e7a55bc17b59870529c1b0f26fd6228
carlosghabrous <carlos.ghabrous@plantix.net>
2023-08-07 08:51:15 +0200

    style: apply fmt

--- main.tf
+++ main.tf
@@ -1,54 +1,54 @@
 terraform {
 
   backend "s3" {
-    bucket = "ghab-tf-course-state"
-    key = "tf-infra/terraform.tfstate"
-    region = "eu-central-1"
+    bucket         = "ghab-tf-course-state"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "eu-central-1"
     dynamodb_table = "terraform-state-locking"
-    encrypt = true
+    encrypt        = true
   }
 
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
 resource "aws_s3_bucket" "ghab_tf_course_state" {
-    bucket = "ghab-tf-course-state"
-    force_destroy = true
+  bucket        = "ghab-tf-course-state"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "state_versioning" {
-    bucket = aws_s3_bucket.ghab_tf_course_state.id
-    versioning_configuration {
-      status = "Enabled"
-    }
+  bucket = aws_s3_bucket.ghab_tf_course_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "state_enc_configuration" {
-    bucket = aws_s3_bucket.ghab_tf_course_state.id 
-    rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
+  bucket = aws_s3_bucket.ghab_tf_course_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-state-locking"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-      name = "LockID"
-      type = "S"
-    }
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 
@@ -69,6 +69,6 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "example" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 }

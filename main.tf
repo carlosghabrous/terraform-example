ae70ab1253e4c54c95d947a2e989744e07b43ddc
carlosghabrous <carlos.ghabrous@plantix.net>
2023-08-07 08:49:17 +0200

    feat: use remote s3 backend

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+
+  backend "s3" {
+    bucket = "ghab-tf-course-state"
+    key = "tf-infra/terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
+  }
+
     required_providers {
       aws = {
         source = "hashicorp/aws"

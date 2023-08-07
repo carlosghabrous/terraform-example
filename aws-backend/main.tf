c98241ea7e33358f1909dbad98fb79a44751cd56
carlosghabrous <carlos.ghabrous@plantix.net>
2023-08-07 17:29:24 +0200

    refac: move file

--- aws-backend/main.tf
+++ aws-backend/main.tf
@@ -50,25 +50,3 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-}

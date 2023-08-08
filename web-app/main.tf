da3e4f3fbb308a89b98ce65a49ab204cd87160ba
carlosghabrous <carlos.ghabrous@plantix.net>
2023-08-08 15:12:23 +0200

    add more resources

--- web-app/main.tf
+++ web-app/main.tf
@@ -60,6 +60,27 @@ resource "aws_instance" "instance_2" {
   EOF
 }
 
+resource "aws_s3_bucket" "instances_bucket" {
+  bucket = "instances-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "instances_bucket_ver" {
+  bucket = aws_s3_bucket.instances_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_enc_configuration" {
+  bucket = aws_s3_bucket.instances_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 data "aws_vpc" "default_vpc" {
   default = true
 }
@@ -68,6 +89,7 @@ data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
 
+# to allow inbound traffic 
 resource "aws_security_group" "instances" {
   name = "instance-security-group"
 }
@@ -82,6 +104,7 @@ resource "aws_security_group_rule" "allow_http_inbound" {
     cird_blocks = ["0.0.0.0/0"]
 }
 
+// load balancer
 resource "aws_alb_listener" "http" {
     load_balancer_arn = aws_lb.load_balancer.load_balancer_arn
 
@@ -93,7 +116,42 @@ resource "aws_alb_listener" "http" {
     }
 
     fixed_response {
-        content-type
+        content_type = "text/plain"
+        message_body = "404: page not found"
+        status_code = 404
     }
 }
+
+resource "aws_lb_target_group" "instances" {
+  name = "example-target-group"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = aws_vpc.default_vpc.id
+
+  healthcheck {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance_1" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id = aws_instance.instance_1.id
+  port = 8080
+
+}
+
+resource "aws_lb_target_group_attachment" "instance_2" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id = aws_instance.instance_2.id
+  port = 8080
+
+}
+
+
 

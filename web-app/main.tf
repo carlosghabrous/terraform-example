de7a40be793a35f1bb3031085c1e9f4e56d7f584
carlosghabrous <carlos.ghabrous@plantix.net>
2023-08-09 15:44:43 +0200

    refac: add ec2 instances module

--- web-app/main.tf
+++ web-app/main.tf
@@ -21,28 +21,6 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "instance_1" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 1" > index.html
-              python3 -m http.server 8080 &
-              EOF
-}
-
-resource "aws_instance" "instance_2" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 2" > index.html
-              python3 -m http.server 8080 &
-              EOF
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "devops-directive-web-app-data"
   force_destroy = true

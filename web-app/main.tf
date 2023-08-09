8eaf5a0413d395294190b2df107b139d0383acf8
carlosghabrous <carlos.ghabrous@plantix.net>
2023-08-09 15:57:08 +0200

    refac: add dns file

--- web-app/main.tf
+++ web-app/main.tf
@@ -1,38 +1,8 @@
 terraform {
-  # Assumes s3 bucket and dynamo DB table already set up
-  # See /code/03-basics/aws-backend
-  backend "s3" {
-    bucket         = var.bucket_name
-    key            = "03-basics/web-app/terraform.tfstate"
-    region         = var.region
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_route53_zone" "primary" {
-  name = var.domain
-}
-
-resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = var.domain
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.load_balancer.dns_name
-    zone_id                = aws_lb.load_balancer.zone_id
-    evaluate_target_health = true
   }
 }
